84ac96e2264c6305cd456ddc4411cb5e1b34a9d0
wolfix1234 <130220073+wolfix1234@users.noreply.github.com>
2025-08-25 15:55:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const port = process.env.PORT || 3005;
 const server = http.createServer((req, res) => {
-  res.end('Hello from Jenkins CI/CD on Kubernetessss!\n');
+  res.end('Hello from Jenkins CI/CD on Kubernetesssss!\n');
 });
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
